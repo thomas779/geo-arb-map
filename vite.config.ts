0e96abd508f25aecfd02befac6c0145fa7be38b2
Thomas <57649600+thomas779@users.noreply.github.com>
2026-07-16 20:24:32 +0100

    fix: isProduction key

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/geo-arb-map/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
